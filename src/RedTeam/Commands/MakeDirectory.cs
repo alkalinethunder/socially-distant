68b090865a3a3d73cb72728cea3a776a5f0f2be9
Michael VanOverbeek <theplexgate@gmail.com>
2021-03-21 00:38:04 -0400

    mkdir

---
+++ src/RedTeam/Commands/MakeDirectory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace RedTeam.Commands
+{
+    public class MakeDirectory : Command
+    {
+        public override string Name => "mkdir";
+
+        protected override void Main(string[] args)
+        {
+            if (!args.Any())
+            {
+                Console.WriteLine("{0}: usage: {0} <path>", Name);
+                return;
+            }
+
+            var path = ResolvePath(args.First());
+
+            try
+            {
+                FileSystem.CreateDirectory(path);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("{0}: {1}: {2}", Name, path, ex.Message);
+            }
+        }
+    }
+}
