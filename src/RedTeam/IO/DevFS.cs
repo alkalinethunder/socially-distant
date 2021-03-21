2b22d4ac9b0748c06d00430f1b11d94205877845
Michael VanOverbeek <theplexgate@gmail.com>
2021-03-20 23:32:17 -0400

    /dev/null

--- src/RedTeam/IO/DevFS.cs
+++ src/RedTeam/IO/DevFS.cs
@@ -1,29 +1,31 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 
 namespace RedTeam.IO
 {
-    public class WindowsPseudoNode : Node
+    public class DevFS : Node
     {
+        private Node _parent;
+
+        public DevFS(Node parent)
+        {
+            _parent = parent;
+        }
+
         public override bool CanRead => false;
         public override bool CanWrite => false;
         public override bool CanExecute => false;
         public override bool CanList => true;
         public override bool CanCreate => false;
-        public override Node Parent => null;
-        public override string Name => string.Empty;
+        public override Node Parent => _parent;
 
         public override IEnumerable<Node> Children
         {
             get
             {
-                foreach (var drive in DriveInfo.GetDrives())
-                {
-                    yield return new WindowsDriveNode(this, drive);
-                }
-
-                yield return new DevFS(this);
+                yield return new NullNode(this);
             }
         }
+
+        public override string Name => "dev";
     }
 }
