2b7d95adf7f3d5d4ee8d523742bf07bfa60d8abd
Michael VanOverbeek <theplexgate@gmail.com>
2021-03-21 01:34:20 -0400

    whoami

---
+++ src/RedTeam/Commands/WhoAmI.cs
@@ -0,0 +1,12 @@
+﻿namespace RedTeam.Commands
+{
+    public class WhoAmI : Command
+    {
+        public override string Name => "whoami";
+
+        protected override void Main(string[] args)
+        {
+            Console.WriteLine(Context.UserName);
+        }
+    }
+}
