92132bd9359d802440f5a2209121441ef04924f7
Michael VanOverbeek <theplexgate@gmail.com>
2021-03-21 18:01:12 -0400

    platform information inside in-game neofetch

--- src/RedTeam/Commands/NeoFetch.cs
+++ src/RedTeam/Commands/NeoFetch.cs
@@ -1,16 +1,63 @@
-﻿namespace RedTeam.Commands
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RedTeam.Commands
 {
     public class NeoFetch : Command
     {
         public override string Name => "neofetch";
         protected override void Main(string[] args)
         {
-            for (var i = 0; i < 16; i++)
-            {
-                var hex = i.ToString("x");
-                Console.WriteLine(ConsoleControl.BackgroundColorCode + hex + "    " +
-                                  ConsoleControl.BackgroundColorCode + "0");
-            }
+            var template = GetTemplate();
+            var xnaAsm = typeof(Microsoft.Xna.Framework.Game).Assembly;
+            var user = $"{Context.UserName}@{Context.HostName}";
+            var line = Repeat('-', user.Length);
+            var host = RedTeamPlatform.OSName;
+            var version = this.GetType().Assembly.GetName().Version.ToString();
+            var kernelname = xnaAsm.GetName().Name;
+            var kernelversion = xnaAsm.GetName().Version.ToString();
+            var gpu = GraphicsAdapter.DefaultAdapter.Description;
+            var memUsed = (GC.GetTotalMemory(false) / 1024 / 1024).ToString();
+            var memTotal = RedTeamPlatform.GetTotalSystemMemory().ToString();
+            var cpu = RedTeamPlatform.GetProcessorName();
+
+            Substitute(ref template, nameof(cpu), cpu);
+            Substitute(ref template, nameof(user), user);
+            Substitute(ref template, nameof(line), line);
+            Substitute(ref template, nameof(version), version);
+            Substitute(ref template, nameof(host), host);
+            Substitute(ref template, nameof(kernelname), kernelname);
+            Substitute(ref template, nameof(kernelversion), kernelversion);
+            Substitute(ref template, nameof(gpu), gpu);
+            Substitute(ref template, nameof(memUsed), memUsed);
+            Substitute(ref template, nameof(memTotal), memTotal);
+            
+            Console.WriteLine(template);
+        }
+
+        private string Repeat(char ch, int len)
+        {
+            var str = "";
+            for (var i = 0; i < len; i++)
+                str += ch;
+            return str;
+        }
+        
+        private void Substitute(ref string str, string sub, string value)
+        {
+            str = str.Replace($"{{{sub}}}", value);
+        }
+        
+        private string GetTemplate()
+        {
+            var asm = this.GetType().Assembly;
+            var resource = asm.GetManifestResourceStream("RedTeam.Resources.NeoFetchTemplate.txt");
+            using var reader = new StreamReader(resource, Encoding.UTF8, true);
+            return reader.ReadToEnd();
         }
     }
 }
--- src/RedTeam/RedTeamPlatform.cs
+++ src/RedTeam/RedTeamPlatform.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
 using BindingFlags = System.Reflection.BindingFlags;
 
 namespace RedTeam
 {
     public static class RedTeamPlatform
     {
+        public static string OSName
+            => Environment.OSVersion.VersionString;
+        
         public static Platform GetCurrentPlatform()
         {
             switch (Environment.OSVersion.Platform)
@@ -42,6 +50,71 @@ namespace RedTeam
                     if (type.GetConstructor(Type.EmptyTypes) != null)
                         yield return type;
                 }
+            }
+        }
+
+        public static Process ExecProcess(string path, string[] args)
+        {
+            var startInfo = new ProcessStartInfo();
+            startInfo.FileName = path;
+            foreach (var arg in args)
+                startInfo.ArgumentList.Add(arg);
+            startInfo.RedirectStandardOutput = true;
+
+            return Process.Start(startInfo);
+        }
+
+        public static string GetProcessorName()
+        {
+            // thank you, John Tur.
+            if (X86Base.IsSupported)
+            {
+                Span<int> text = stackalloc int[3 * 4];
+                for (var i = 0; i < 3; i++)
+                {
+                    (text[i * 4 + 0], 
+                        text[i * 4 + 1], 
+                        text[i * 4 + 2], 
+                        text[i * 4 + 3]) = X86Base.CpuId((int) (i + 0x80000002), 0);
+                }
+
+                return Encoding.ASCII.GetString(MemoryMarshal.Cast<int, byte>(text));
+            }
+            else
+            {
+                return "Unknown";
+            }
+        }
+        
+        /// <summary>
+        /// Retrieves the amount of total system memory in megabytes.
+        /// </summary>
+        /// <returns>Amount of system memory in megabytes.</returns>
+        public static long GetTotalSystemMemory()
+        {
+            if (IsPlatform(Platform.Windows))
+            {
+                using var proc = ExecProcess("wmic", new[] {"OS", "get", "TotalVisibleMemorySize", "/Value"});
+                var output = proc.StandardOutput.ReadToEnd();
+                
+                var trim = output.Trim();
+                var split = trim.Split('=').Last();
+                var kbytes = long.Parse(split);
+
+                return kbytes / 1024;
+            }
+            else
+            {
+                using var proc = ExecProcess("/bin/bash", new[] {"-c", "free -m"});
+                var output = proc.StandardOutput.ReadToEnd();
+
+                var lines = output.Split('\n');
+                var memLine = lines.First(x => x.StartsWith("Mem:"));
+                var cols = memLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var total = cols[1];
+                var mbytes = long.Parse(total);
+
+                return mbytes;
             }
         }
     }
