db5aaf0fb81ad29625888cba2a0939bd34238e09
Michael VanOverbeek <theplexgate@gmail.com>
2021-06-12 08:18:02 -0400

    continue working on the settings screen.

--- src/RedTeam/SettingsComponent.cs
+++ src/RedTeam/SettingsComponent.cs
@@ -37,13 +37,23 @@ namespace RedTeam
 
         // Graphics Settings UI
         private Stacker _graphicsStacker = new();
-        
+        private LabeledDropdown _resolution = new();
+        private SettingToggle _windowMode = new();
+        private LabeledDropdown _consoleFontSize = new();
+        private LabeledDropdown _guiScale = new();
+        private SettingToggle _bloom = new();
+        private SettingToggle _shadowmask = new();
+        private SettingToggle _glitches = new();
+        private SettingToggle _vsync = new();
+
         // Audio Settings UI
         private Stacker _audioStacker = new();
         
         // Gameplay Settings UI
         private Stacker _gameplayStacker = new();
-        
+        private LabeledDropdown _primaryMouse = new();
+        private LabeledDropdown _consolePalette = new();
+
         // About UI
         private Stacker _aboutStacker = new();
         
@@ -77,7 +87,10 @@ namespace RedTeam
             _mainScroller.Properties.SetValue(Stacker.FillProperty, true);
 
             // Padding
-            _masterStacker.Padding = 15;
+            _masterStacker.Padding = 10;
+            _sidebarScroller.Padding = 5;
+            _mainScroller.Padding = 5;
+            _buttonList.Padding = 5;
             
             // Set up the button list.
             _buttonList.Children.Add(_restoreDefaults);
@@ -102,34 +115,128 @@ namespace RedTeam
 
         private void BuildGraphicsUI()
         {
-            var resolutionSelector = new LabeledDropdown();
-            var fullscreenMode = new LabeledDropdown();
+            var displayHeader = new TextBlock();
+            var ppHeader = new TextBlock();
+            var guiHeader = new TextBlock();
 
-            fullscreenMode.Title = "Window mode";
-            resolutionSelector.Title = "Screen Resolution";
+            displayHeader.Text = "Display";
+            ppHeader.Text = "Effects";
+            guiHeader.Text = "User Interface";
 
-            var windowStacker = new Stacker();
-            windowStacker.Direction = StackDirection.Horizontal;
+            displayHeader.Font = App.Content.Load<SpriteFont>("Fonts/MenuTitle");
+            ppHeader.Font = App.Content.Load<SpriteFont>("Fonts/MenuTitle");
+            guiHeader.Font = App.Content.Load<SpriteFont>("Fonts/MenuTitle");
 
-            windowStacker.Children.Add(resolutionSelector);
-            windowStacker.Children.Add(fullscreenMode);
+            displayHeader.Color = Color.Cyan;
+            ppHeader.Color = Color.Cyan;
+            guiHeader.Color = Color.Cyan;
 
-            resolutionSelector.Padding = 30;
-            fullscreenMode.Padding = 30;
+            _resolution.Title = "Screen Resolution";
+            _windowMode.Title = "Fullscreen";
+            _bloom.Title = "Bloom";
+            _shadowmask.Title = "CRT Shadow-mask";
+            _glitches.Title = "Glitch Effects";
+            _vsync.Title = "Vertical Sync";
+            _guiScale.Title = "GUI Scale";
+            _consoleFontSize.Title = "Console Font Size";
             
+            _bloom.ToolTip = "Bloom effect makes bright text and objects glow.";
+            _shadowmask.ToolTip =
+                "Overlay the screen with a color CRT shadow-mask to increase the 80s retro computer aesthetic.";
+            _glitches.ToolTip = "Screw with the screen when glitch sounds play.";
+            
+            var windowStacker = new WrapPanel();
+            var postProcessStacker = new WrapPanel();
+            var guiStacker = new WrapPanel();
+            
+            windowStacker.Orientation = StackDirection.Horizontal;
+            postProcessStacker.Orientation = StackDirection.Horizontal;
+            guiStacker.Orientation = StackDirection.Horizontal;
+            
+            windowStacker.Children.Add(_resolution);
+            windowStacker.Children.Add(_windowMode);
+            windowStacker.Children.Add(_vsync);
+            
+            postProcessStacker.Children.Add(_bloom);
+            postProcessStacker.Children.Add(_shadowmask);
+            postProcessStacker.Children.Add(_glitches);
+
+            guiStacker.Children.Add(_consoleFontSize);
+            guiStacker.Children.Add(_guiScale);
+            
+            _resolution.Padding = 15;
+            _windowMode.Padding = 15;
+            _vsync.Padding = 15;
+            _bloom.Padding = 15;
+            _shadowmask.Padding = 15;
+            _glitches.Padding = 15;
+            _consoleFontSize.Padding = 15;
+            _guiScale.Padding = 15;
+            
+            _graphicsStacker.Children.Add(displayHeader);
             _graphicsStacker.Children.Add(windowStacker);
+            _graphicsStacker.Children.Add(ppHeader);
+            _graphicsStacker.Children.Add(postProcessStacker);
+            _graphicsStacker.Children.Add(guiHeader);
+            _graphicsStacker.Children.Add(guiStacker);
+            
+            _windowMode.IsChecked = _config.ActiveConfig.IsFullscreen;
+            _vsync.IsChecked = _config.ActiveConfig.VSync;
+            _bloom.IsChecked = _config.ActiveConfig.Effects.Bloom;
+            _shadowmask.IsChecked = _config.ActiveConfig.Effects.ShadowMask;
 
             var i = 0;
             foreach (var displayMode in GraphicsAdapter.DefaultAdapter.SupportedDisplayModes
                 .OrderByDescending(x => x.Width * x.Height).Select(x => $"{x.Width}x{x.Height}").Distinct())
             {
-                resolutionSelector.AddItem(displayMode);
+                _resolution.AddItem(displayMode);
                 if (displayMode == $"{_config.GetDisplayMode().Width}x{_config.GetDisplayMode().Height}")
                 {
-                    resolutionSelector.SelectedIndex = i;
+                    _resolution.SelectedIndex = i;
                 }
                 i++;
             }
+            
+            // Bind resolution change event.
+            _resolution.SelectedIndexChanged += ResolutionOnSelectedIndexChanged;
+            _windowMode.CheckStateChanged += WindowModeOnCheckStateChanged;
+            _bloom.CheckStateChanged += BloomOnCheckStateChanged;
+            _shadowmask.CheckStateChanged += ShadowmaskOnCheckStateChanged;
+            _vsync.CheckStateChanged += VsyncOnCheckStateChanged;
+        }
+
+        private void VsyncOnCheckStateChanged(object? sender, EventArgs e)
+        {
+            _config.ActiveConfig.VSync = _vsync.IsChecked;
+            _config.ApplyChanges();
+        }
+
+        private void ShadowmaskOnCheckStateChanged(object? sender, EventArgs e)
+        {
+            _config.ActiveConfig.Effects.ShadowMask = _shadowmask.IsChecked;
+            _config.ApplyChanges();
+        }
+
+        private void BloomOnCheckStateChanged(object? sender, EventArgs e)
+        {
+            _config.ActiveConfig.Effects.Bloom = _bloom.IsChecked;
+            _config.ApplyChanges();
+        }
+
+        private void WindowModeOnCheckStateChanged(object? sender, EventArgs e)
+        {
+            _config.ActiveConfig.IsFullscreen = _windowMode.IsChecked;
+            _config.ApplyChanges();
+        }
+
+        private void ResolutionOnSelectedIndexChanged(object? sender, EventArgs e)
+        {
+            // Get the desired resolution.
+            var res = _resolution.SelectedItem;
+            
+            // Give it to Thundershock!
+            _config.SetDisplayMode(res);
+            _config.ApplyChanges();
         }
 
         private void UpdateCategories()
